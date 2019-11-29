bcb517441e6ad21b234a6761a06e137e8d30ac62
gavinrz4177 <gavinrz4177@users.noreply.github.com>
2019-11-29 08:20:27 -0800

    Initial commit

---
+++ src/main/java/com/z/y/shorterways/ShorterwaysApplication.java
@@ -0,0 +1,13 @@
+package com.z.y.shorterways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorterwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorterwaysApplication.class, args);
+	}
+
+}
