77c223f246b19b3a9831335aeb1dbb431aa1fd1f
gavinrz4177 <gavinrz4177@users.noreply.github.com>
2019-11-29 09:22:03 -0800

    1day for hello word

---
+++ src/main/java/com/z/y/shorterways/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.z.y.shorterways.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("")
+	public String index(Model model) {
+		model.addAttribute("message", "shortways.com");
+		return "index";
+	}
+}
